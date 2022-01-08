b13702f7fea48f7aea38f58282ac018d4137d8d4
Sleepeels <Lior.webdev@gmail.com>
2022-01-08 13:59:42 +0200

    chat-app: backend

--- server.js
+++ server.js
@@ -1,29 +1,40 @@
 const app = require("express")();
 const http = require("http").Server(app);
-const { timeStamp } = require("console");
-const cors = require("cors");
-const PORT = process.env.PORT || 5000;
-// **************************** //
+
+const PORT = 5000;
+
 const options = {
   cors: true,
-  // origins: ["http://127.0.0.1:3000"],
 };
-const io = require("socket.io")(http, options);
-// const io = require("socket.io")(http);
 
-// http.use(cors());
+const io = require("socket.io")(http, options);
+
+const chatHistory = [];
 
 io.on("connection", (socket) => {
-  console.log("NEW socket >> " + socket.id + ":" + Date.now().toLocaleString());
-  socket.on("message", ({ name, message }) => {
-    io.emit("message", { name, message });
+  console.log("NEW socket >> " + socket.id);
+
+  socket.on("join-chat-room", (data) => {
+    socket.join(data.room);
+    // console.log(`${data.name}:<${socket.id}> has join room ${data.room} `);
+    const found = chatHistory.find((his) => his.room == data.room);
+    if (found) {
+      socket.emit("get-history", found.msgs);
+    }
   });
-  socket.on("disconnect", function () {
-    console.log("user disconnected" + socket.id);
+
+  socket.on("send-message", (data) => {
+    const found = chatHistory.find((his) => his.room == data.room);
+    if (found) found.msgs.push(data);
+    else chatHistory.push({ room: data.room, msgs: [data] });
+    socket.to(data.room).emit("recieve-message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("DEAD socket >> " + socket.id);
   });
 });
 
-// **************************** //
 http.listen(PORT, () => {
   console.log(`listening on post ${PORT}...`);
 });
